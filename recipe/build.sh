d27a91a043dab508e52a34aebdc13e0e22bcaa82
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-05-30 11:39:40 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 export CXX=$GXX # the sphinx configure script does not recognise x86_64-conda_cos6-linux-gnu-c++ as g++ so we replace it with x86_64-conda_cos6-linux-gnu-g++ which works fine. 
-./configure OBJCXX=${CXX} --disable-debug --with-sxmath --enable-mkl --prefix=${PREFIX} --with-mklpath=${PREFIX} --enable-mklfft --enable-pcre2
+./configure --prefix=${PREFIX} \
+    OBJCXX=${CXX} \
+    CFLAGS=${CFLAGS} \
+    CPPFLAGS=${CPPFLAGS} \ 
+    --enable-mkl --enable-mklfft --with-mklpath=${PREFIX} \
+    --disable-debug --with-sxmath --enable-pcre2
 make all -j${CPU_COUNT}
 make install 
