86b1eda54a77a2afcc3c4fb6316ed4c0e078635d
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-05-30 12:22:13 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,7 @@ export CXX=$GXX # the sphinx configure script does not recognise x86_64-conda_co
 ./configure --prefix=${PREFIX} \
     OBJCXX=${CXX} \
     CFLAGS="${CFLAGS} -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_core" \
-    CPPFLAGS="${CPPFLAGS} -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_core" \ 
+    CPPFLAGS="${CPPFLAGS} -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_core" \
     --enable-mkl --enable-mklfft --with-mklpath=${PREFIX} \
     --disable-debug --with-sxmath --enable-pcre2
 make all -j${CPU_COUNT}
