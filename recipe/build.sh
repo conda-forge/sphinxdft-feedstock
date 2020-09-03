c66feb917e093d6266ace4aff7390e63c12bb774
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-09-03 16:24:12 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,7 @@ export CXX=$GXX # the sphinx configure script does not recognise x86_64-conda_co
 ./configure --prefix=${PREFIX} \
     OBJCXX=${CXX} \
     CFLAGS="${CFLAGS}" \
-    CPPFLAGS="${CPPFLAGS} -Wno-literal-suffix" \
+    CPPFLAGS="${CPPFLAGS} -Wno-literal-suffix -D__STDC_FORMAT_MACROS" \
     --enable-mkl --enable-mklfft --with-mklpath=${PREFIX} \
     --disable-debug --with-sxmath --enable-pcre2
 make all -j${CPU_COUNT}
